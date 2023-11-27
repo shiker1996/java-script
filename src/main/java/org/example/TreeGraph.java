147b0a8a87e5100dfb5333db3e1a226c99b50c21
shker <1159181016@qq.com>
2023-11-27 19:52:56 +0800

    java常用脚本文件

---
+++ src/main/java/org/example/TreeGraph.java
@@ -0,0 +1,98 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//将列表转换为树并进行可视化
+public class TreeGraph {
+
+    static class Node {
+        Long id;
+        String nodeName;
+        int level;
+        String nodeProperties;
+        List<Node> children = new ArrayList<>();
+
+        Node(Long id, String nodeName, int level, String nodeProperties) {
+            this.id = id;
+            this.nodeName = nodeName;
+            this.level = level;
+            this.nodeProperties = nodeProperties;
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File("/Users/001/Downloads/test.csv");
+        if (!file.isFile() || !file.exists()) {
+            System.out.println("文件不存在!");
+            return;
+        }
+        List<String[]> relationships = new ArrayList<>();
+        try (InputStreamReader isr = new InputStreamReader(Files.newInputStream(file.toPath()), StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(isr)) {
+            String lineTxt;
+            while ((lineTxt = br.readLine()) != null) {
+                String[] data = lineTxt.split(",");
+                String id = data[0].trim();
+                String parentId = data[1].trim();
+                String fromMerchantNo = data[2].trim();
+                String toMerchantNo = data[3].trim();
+                String level = data[4].trim();
+                String amount = data[5].trim();
+                relationships.add(new String[]{parentId, id, fromMerchantNo,toMerchantNo, level, amount});
+            }
+        } catch (Exception e) {
+            System.out.println("导入数据错误");
+        }
+        printList(relationships);
+    }
+
+    private static void printList(List<String[]> relationships){
+        if(relationships == null||relationships.isEmpty()){
+            return;
+        }
+        Map<Long, Node> nodes = new HashMap<>();
+
+        for (String[] relationship : relationships) {
+            Long id = Long.valueOf(relationship[0]);
+            String nodeName = relationship[2];
+            int nodeLevel = Integer.parseInt(relationship[4]);
+            nodes.put(id, new Node(id, nodeName, nodeLevel, null));
+        }
+
+        for (String[] relationship : relationships) {
+            Long id = Long.valueOf(relationship[1]);
+            String nodeName = relationship[3];
+            int nodeLevel = Integer.parseInt(relationship[4]);
+            nodes.put(id, new Node(id, nodeName, nodeLevel + 1,relationship[5]));
+        }
+
+        for (String[] relationship : relationships) {
+            String parentId = relationship[0];
+            String childId = relationship[1];
+            nodes.get(Long.valueOf(parentId)).children.add(nodes.get(Long.valueOf(childId)));
+        }
+        for (Node node: nodes.values()) {
+            if(node.level == 1) {
+                printTree(node, "", true);
+            }
+        }
+    }
+
+    private static void printTree(Node node, String prefix, boolean isTail) {
+        System.out.println(prefix + (isTail ? "└── " : "├── ") + node.nodeName + "(" + node.nodeProperties + ")");
+        for (int i = 0; i < node.children.size() - 1; i++) {
+            printTree(node.children.get(i), prefix + (isTail ? "    " : "│   "), false);
+        }
+        if (!node.children.isEmpty()) {
+            printTree(node.children.get(node.children.size() - 1), prefix + (isTail ? "    " : "│   "), true);
+        }
+    }
+}
